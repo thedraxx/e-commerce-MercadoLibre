31696cd62232e28e18ec4de10acb2334d3362ea3
thedraxx <thedraxxgames@gmail.com>
2023-04-05 13:44:10 -0300

    fix array images

--- components/products/ProductSlideshow.tsx
+++ components/products/ProductSlideshow.tsx
@@ -10,14 +10,32 @@ interface Props {
 }
 
 export const ProductSlideshow: FC<Props> = ({ product }: Props) => {
-    const [arrayImages, setArrayImages] = useState([]);
+
+
+    const [arrayImages, setArrayImages] = useState<string[]>([]);
+
+    console.log('arrayImages', arrayImages)
 
     useEffect(() => {
-        product.imagen1 && setArrayImages([...arrayImages, product.imagen1] as any);
-        product.imagen2 && setArrayImages([...arrayImages, product.imagen2] as any);
-        product.imagen3 && setArrayImages([...arrayImages, product.imagen3] as any);
-        product.imagen4 && setArrayImages([...arrayImages, product.imagen4] as any);
-    }, [])
+
+        if (product.imagen1 !== '') {
+            setArrayImages([...arrayImages, product.imagen1] as string[]);
+        }
+
+        if (product.imagen2 !== '') {
+            setArrayImages([...arrayImages, product.imagen2] as string[]);
+        }
+
+        if (product.imagen3 !== '') {
+            setArrayImages([...arrayImages, product.imagen3] as string[]);
+        }
+
+        if (product.imagen4 !== '') {
+            setArrayImages([...arrayImages, product.imagen4] as string[]);
+        }
+
+
+    }, [product])
 
     return (
         <Slide
@@ -27,7 +45,7 @@ export const ProductSlideshow: FC<Props> = ({ product }: Props) => {
 
         >
             {
-                arrayImages.map(image => {
+                [product.imagen1, product.imagen2, product.imagen3, product.imagen4].map(image => {
                     console.log('', image)
                     return (
                         <div
@@ -39,6 +57,10 @@ export const ProductSlideshow: FC<Props> = ({ product }: Props) => {
                                 style={{
                                     backgroundImage: `url(${image})`,
                                     backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    height: '100%',
+                                    width: '100%',
+                                    backgroundRepeat: 'no-repeat',
 
                                 }}>
                             </div>
